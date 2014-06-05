3edd7f5743a97434a6e6cf2f0587aa76d5068018
Ehud Magal <ehudmagal@gmail.com>
2014-06-05 12:12:38 +0300

    static files dir in settings.py

--- settings.py
+++ settings.py
@@ -30,3 +30,5 @@ MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media')
 TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), 'templates'),)
 
 ROOT_URLCONF = 'urls'
+
+STATIC_URL = '/admin/'
